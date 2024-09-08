5ec4629b13ae748a4e3b32fb166f1abf146c666d
Mayur-tiwari <119853809+Mayur-tiwari@users.noreply.github.com>
2024-09-08 16:38:10 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-# Pull the Docker image from Docker Hub
+# Pulling the Docker image from Docker Hub
 docker pull mayurtiwari/simple-python-app
 
 # Run the Docker image as a container
